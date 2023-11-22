1f42c9eac4b8c686685132d8071d55c117ba947e
Rah <arrehman757@gmail.com>
2023-11-22 21:20:17 +0530

    Added turtle and positioned it at start

--- player.py
+++ player.py
@@ -1,7 +1,14 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('black')
+        self.shape('turtle')
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
